9fa44a2c7bfd271061fe6cb5ea3537e741c87466
Ricardo Arruda Sowek <ricardaotsi@gmail.com>
2018-05-08 08:57:57 -0300

    Update web scraping

--- Web scraping/InterativaContadores.py
+++ Web scraping/InterativaContadores.py
@@ -17,7 +17,7 @@ impressoras=["172.17.10.21",
 
 scope = ['https://spreadsheets.google.com/feeds']
 credentials = ServiceAccountCredentials.from_json_keyfile_name(
-    "/path/to/json", scope)
+    "/home/ti/Documentos/git/simpress-5e35fc2d6c71.json", scope)
 gc = gspread.authorize(credentials)
 
 sheet=1
@@ -26,26 +26,32 @@ for imp in impressoras:
     try:
         page1 = requests.get("http://"+imp+"/web/guest/br/websys/status/getUnificationCounter.cgi")
         page1.raise_for_status()
-        soup = BeautifulSoup(page1.content, 'html5lib')
-        contador=soup.find_all("tr", class_="staticProp")
-        result_contador = list(contador[1])
-
         page2 = requests.get("http://"+imp+"/web/guest/br/webprinter/supply.cgi")
         page2.raise_for_status()
-        soup = BeautifulSoup(page2.content,'html5lib')
+
+    except requests.exceptions.RequestException as err:
+        with open("/home/ti/Documentos/git/log", "a") as myfile:
+            myfile.write(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")+" : "+imp + " " + str(err) + "\n \n")
+
+    else:
+        soup = BeautifulSoup(page1.content, 'html5lib')
+        contador = soup.find_all("tr", class_="staticProp")
+        result_contador = list(contador[1])
+
+        soup = BeautifulSoup(page2.content, 'html5lib')
         suprimento = soup.find_all("td", class_="black")
-        result_suprimento = int((int(str(suprimento[0])[84:-8])*100)/162)
+        result_suprimento = int((int(str(suprimento[0])[84:-8]) * 100) / 162)
 
         gspreadlist = [
             datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S"),
             str(result_contador[3])[14:-5],
-            str(result_suprimento)+"%"
+            str(result_suprimento) + "%"
         ]
 
         wks = gc.open("Contadores").get_worksheet(sheet)
         wks.insert_row(gspreadlist, index=3)
 
-        sheet += 1
-    except requests.exceptions.RequestException as err:
-        with open("/path/to/log", "a") as myfile:
-            myfile.write(imp + " " + str(err) + "\n")
+        with open("/home/ti/Documentos/git/log", "a") as myfile:
+            myfile.write(datetime.datetime.now().strftime("%d/%m/%y %H:%M:%S")+" : "+imp+" DONE \n \n")
+
+    sheet += 1
