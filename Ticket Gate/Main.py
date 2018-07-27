be385dd7c6ef5180b6d6033fe8b83b8d1bb40e3c
Ricardo Arruda Sowek <ricardo.sowek@gmail.com>
2018-07-27 08:31:15 -0300

    Ticket Gate atualizado

--- Ticket Gate/Main.py
+++ Ticket Gate/Main.py
@@ -12,37 +12,37 @@ file=open( open_path, "r")
 reader = csv.reader(file)
 for line in reader:
     #Tab personal data
-    pyautogui.click(88, 258)
+    pyautogui.click(77, 271)
     pyautogui.PAUSE = 0.2
-    pyautogui.click(140, 288); pyautogui.typewrite(line[1])#Name
+    pyautogui.click(133, 304); pyautogui.typewrite(line[1])#Name
     pyautogui.PAUSE = 0.2
-    pyautogui.click(140, 345); pyautogui.typewrite(line[0])#ID
+    pyautogui.click(136, 363); pyautogui.typewrite(line[0])#ID
     pyautogui.PAUSE = 0.2
-    pyautogui.click(140, 374); pyautogui.typewrite(line[2])#TIcket
+    pyautogui.click(136, 390); pyautogui.typewrite(line[2])#TIcket
     pyautogui.PAUSE = 0.2
-    pyautogui.click(500, 345); pyautogui.typewrite(line[3])#Type
+    pyautogui.click(519, 360); pyautogui.typewrite(line[3])#Type
     #Tab Company
-    pyautogui.click(370, 255)
-    pyautogui.click(397, 288)#Button serach company
+    pyautogui.click(402, 271)
+    pyautogui.click(400, 303)#Button serach company
     pyautogui.PAUSE = 0.3
     pyautogui.press('enter')#enter to search company
     pyautogui.PAUSE = 0.3#delay to search
-    pyautogui.click(466, 345)#Select company
-    pyautogui.click(466, 345)#Select company
+    pyautogui.click(448, 364)#Select company
+    pyautogui.click(448, 364)#Select company
     #Tab Access Level
-    pyautogui.click(513, 257)
-    pyautogui.click(309, 290)#Button search access Level
+    pyautogui.click(573, 267)
+    pyautogui.click(320, 300)#Button search access Level
     pyautogui.press('enter')#enter to search access Level
     pyautogui.PAUSE = 0.4#delay to search
-    pyautogui.click(379, 325)#Select access Level (379/345 servidor)(379/364 terceirizado)(379,325 aluno)
+    pyautogui.click(379, 339)#Select access Level (379/360 servidor)(379/384 terceirizado)(379,339 aluno)
     pyautogui.PAUSE = 0.3
-    pyautogui.click(353, 287)#Add selection
+    pyautogui.click(359, 302)#Add selection
     pyautogui.PAUSE = 1.0
     #Button save
-    pyautogui.click(163, 619)
+    pyautogui.click(140, 631)
     pyautogui.PAUSE = 0.5#delay to save
     #Button confirmation
-    pyautogui.click(634, 599)
+    pyautogui.click(640, 609)
     pyautogui.PAUSE = 0.5
 # while True:
 #         x, y = pyautogui.position()
