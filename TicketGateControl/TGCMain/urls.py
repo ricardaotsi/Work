0168d16e4a10b094c85391b6582af7df3fe00255
Ricardo Arruda Sowek <ricardaotsi@gmail.com>
2018-12-18 10:43:25 -0200

    Search and Modify forms done

--- TicketGateControl/TGCMain/urls.py
+++ TicketGateControl/TGCMain/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
+    url(r'^edit/$', views.edit, name='edit'),
 ]
--- TicketGateControl/TGCMain/views.py
+++ TicketGateControl/TGCMain/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.db import connections
 from collections import namedtuple
+from django.contrib import messages
 
 def namedtuplefetchall(cursor):
     "Return all rows from a cursor as a namedtuple"
@@ -9,33 +10,45 @@ def namedtuplefetchall(cursor):
     nt_result = namedtuple('Result', [col[0] for col in desc])
     return [nt_result(*row) for row in cursor.fetchall()]
 
+def sql(Nome, Identificador, Cracha, Matricula, Ativo):
+    query = "select * from public.gen_pessoa where fl_ativo='"+Ativo+"' and "
+    with connections['vwcontrol'].cursor() as cursor:
+            cursor.execute(query+"lower(ds_Identificador_aux) like '%"+Identificador.lower()+"%' and lower(nm_pessoa) like '%"+Nome.lower()+"%' and cast(nr_cracha as text) like '%"+Cracha+"%' and cast(nr_matricula as text) like '%"+Matricula+"%'  order by nm_pessoa")
+            rows = namedtuplefetchall(cursor)
+    return rows
+
 @login_required
 def index(request):
-    query = "select * from public.gen_pessoa where fl_ativo='S' and "
-    # if request.GET.get('nome'):
-    #     with connections['vwcontrol'].cursor() as cursor:
-    #         cursor.execute(query+"lower(nm_pessoa) like '%"+request.GET['nome']+"%' order by nm_pessoa")
-    #         trows = namedtuplefetchall(cursor)
-    #     rows = request.GET['nome']
-    #     return render(request, 'TGCMain/index.html', {"rows" : rows})
-    # else:    
-    #     return render(request, 'TGCMain/index.html', {})
-    if request.method == 'GET':
-        Nome = request.GET.get('nome')
-        Cracha = request.GET.get('cracha')
-        Matricula = request.GET.get('matricula')
-        Identificador = request.GET.get('identificador')
-        if Nome is None:
-            Nome = ""
-        if Cracha is None:
-            Cracha = ""
-        if Matricula is None:
-            Matricula = ""
-        if Identificador is None:
-            Identificador = ""
-        with connections['vwcontrol'].cursor() as cursor:
-            cursor.execute(query+"lower(ds_Identificador_aux) like '%"+Identificador+"%' and lower(nm_pessoa) like '%"+Nome+"%' and cast(nr_cracha as text) like '%"+Cracha+"%' and cast(nr_matricula as text) like '%"+Matricula+"%'  order by nm_pessoa")
-            rows = namedtuplefetchall(cursor)
-        return render(request, 'TGCMain/index.html', {"rows" : rows})
+    if request.GET.get('nome') is None:
+        Nome = ""
     else:
-        return render(request, 'TGCMain/index.html', {})
+        Nome = request.GET.get('nome')
+    if request.GET.get('cracha') is None or request.GET.get('cracha').isdigit() == False:
+        Cracha = ""
+    else:
+        Cracha = request.GET.get('cracha')
+    if request.GET.get('matricula') is None or request.GET.get('matricula').isdigit() == False:
+        Matricula = ""
+    else:
+        Matricula = request.GET.get('matricula')
+    if request.GET.get('identificador') is None:
+        Identificador = ""
+    else:
+        Identificador = request.GET.get('identificador')
+    if request.GET.get('ativo') is None:
+        Ativo = "S"
+    else:
+        Ativo = request.GET.get('ativo')
+    rows = sql(Nome, Identificador, Cracha, Matricula, Ativo )
+    return render(request, 'TGCMain/index.html', {"rows" : rows})
+
+@login_required
+def edit(request):
+    if request.method == "GET":
+        if request.GET.get('cracha') != '' or request.GET.get('ativo') != '':
+            row = sql('','',request.GET.get('cracha'),'', request.GET.get('ativo'))
+    if request.method == "POST":
+        row = sql('','',request.GET.get('cracha'),'',request.GET.get('ativo'))
+        messages.error(request, 'Profile details updated.')
+        messages.success(request, 'Profile details updated.')
+    return render(request, 'TGCMain/edit.html', {"row":row})
