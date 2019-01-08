fe10ae5d44222eb43dbe5ce7623c95c9aa63f8f2
Ricardo Arruda Sowek <ricardaotsi@gmail.com>
2019-01-08 10:26:04 -0200

    powershell

---
+++ PowerShell/Desabilitar deepfreeze 1 reboot.ps1
@@ -0,0 +1,10 @@
+﻿$wshell = New-Object -ComObject wscript.shell;
+$wshell.SendKeys('^+%{F6}')
+$wshell.AppActivate('Deep Freeze Standard')
+Sleep 1
+$wshell.SendKeys('Fl@shm4n')
+$wshell.SendKeys('~')
+$wshell.SendKeys('%{N}')
+$wshell.SendKeys('%{R}')
+Sleep 1
+$wshell.SendKeys('~')
---
+++ PowerShell/Habilitar deepfreeze.ps1
@@ -0,0 +1,12 @@
+﻿$wshell = New-Object -ComObject wscript.shell;
+$wshell.SendKeys('^+%{F6}')
+$wshell.AppActivate('Deep Freeze Standard')
+Sleep 1
+$wshell.SendKeys('Fl@shm4n')
+$wshell.SendKeys('~')
+$wshell.SendKeys('%{F}')
+$wshell.SendKeys('%{O}')
+Sleep 1
+$wshell.SendKeys('~')
+Sleep 1
+shutdown -s -t 5
