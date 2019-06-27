734858824191d3a01665c2cd9906715de0d9e538
Ricardo Arruda <ricardo.sowek@gmail.com>
2019-06-27 13:27:47 -0300

    macro remade

--- Ticket Gate/macro.py
+++ Ticket Gate/macro.py
@@ -3,7 +3,7 @@ import pyautogui
 import time
 
 pyautogui.FAILSAFE = True
-pyautogui.PAUSE=1
+pyautogui.PAUSE=0.5
 
 def buscar(i):
     pyautogui.click(723, 240)
@@ -17,7 +17,9 @@ inp2 = input("Matricula para cadastrar: ")
 
 for i in range(4):
     webbrowser.open_new_tab('http://172.17.150.'+str(i+1)+'/')
+    time.sleep(2)
     pyautogui.click(988, 322)
     buscar(inp2)
     pyautogui.click(843, 422,clicks=2)
     pyautogui.typewrite(inp1)
+    pyautogui.click(694, 920)
