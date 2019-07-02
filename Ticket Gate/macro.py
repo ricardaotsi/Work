1ceffbd3f7b9f77187ad0ccf6b232f71983f9bd8
Ricardo Arruda <ricardo.sowek@gmail.com>
2019-07-02 11:54:43 -0300

    macro update and insert

--- Ticket Gate/macro.py
+++ Ticket Gate/macro.py
@@ -6,12 +6,12 @@ pyautogui.FAILSAFE = True
 pyautogui.PAUSE=0.5
 
 def buscar(i):
-    pyautogui.click(723, 240)
     pyautogui.click(674, 389)
     pyautogui.click(957, 384)
     pyautogui.typewrite(i)
     pyautogui.click(806, 472)
 
+inp0 = input("Nome para Cadastrar: ")
 inp1 = input("Cartão para Cadastrar: ")
 inp2 = input("Matricula para cadastrar: ")
 
@@ -19,7 +19,17 @@ for i in range(4):
     webbrowser.open_new_tab('http://172.17.150.'+str(i+1)+'/')
     time.sleep(2)
     pyautogui.click(988, 322)
-    buscar(inp2)
+    pyautogui.click(723, 240)
+    if not inp0:
+        buscar(inp2)
+    else:
+        pyautogui.click(611, 391)
+        pyautogui.click(836, 350)
+        pyautogui.typewrite(inp0)
+        pyautogui.click(839, 385)
+        pyautogui.typewrite(inp2)
+        pyautogui.click(855, 547)
+        pyautogui.click(904, 621)
     pyautogui.click(843, 422,clicks=2)
     pyautogui.typewrite(inp1)
     pyautogui.click(694, 920)
